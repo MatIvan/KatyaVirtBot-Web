3a2045fe55fb594144701d1d0bf6baf478e6248e
MatIvan <mativ.rufous@yandex.ru>
2023-09-13 22:35:16 +0300

    edit hook: all fields

--- src/components/HookEditor/HookEditor.js
+++ src/components/HookEditor/HookEditor.js
@@ -15,12 +15,20 @@ function HookEditor({ newHook }) {
     const [userId, setUserId] = useState(newHook?.userId);
     const [name, setName] = useState(newHook?.name);
     const [url, setUrl] = useState(newHook?.url);
+    const [chat, setChat] = useState(newHook?.condition?.chatName);
+    const [contains, setContains] = useState(newHook?.condition?.contains?.toString());
+    const [startWith, setStartWith] = useState(newHook?.condition?.startWith?.toString());
+    const [sens, setSens] = useState(newHook?.condition?.caseSensitive);
 
     useEffect(() => {
         setId(newHook?.id);
         setUserId(newHook?.userId);
         setName(newHook?.name);
         setUrl(newHook?.url);
+        setChat(newHook?.condition?.chatName);
+        setContains(newHook?.condition?.contains?.toString());
+        setStartWith(newHook?.condition?.startWith?.toString());
+        setSens(newHook?.condition?.caseSensitive);
     }, [newHook]);
 
     const onSave = () => {
@@ -28,10 +36,10 @@ function HookEditor({ newHook }) {
         const hook = {
             id, userId, name, url,
             condition: {
-                chatName: "",
-                contains: [],
-                startWith: [],
-                caseSensitive: false
+                chatName: chat,
+                contains: contains?.split(',').map(s => s.trim()),
+                startWith: startWith?.split(',').map(s => s.trim()),
+                caseSensitive: sens === undefined ? false : sens
             }
         }
         console.log(hook);
@@ -44,6 +52,10 @@ function HookEditor({ newHook }) {
             <HookEditorLine key='line-userid' caption='userId:' value={userId} readOnly={true} />
             <HookEditorLine key='line-name' caption='name:' value={name} onChange={(newName) => { setName(newName) }} />
             <HookEditorLine key='line-url' caption='url:' value={url} onChange={(newUrl) => { setUrl(newUrl) }} />
+            <HookEditorLine key='line-chat' caption='chat:' value={chat} onChange={(newChat) => { setChat(newChat) }} />
+            <HookEditorLine key='line-contains' caption='contains:' value={contains} onChange={(newContains) => { setContains(newContains) }} />
+            <HookEditorLine key='line-startWith' caption='startWith:' value={startWith} onChange={(newStartWith) => { setStartWith(newStartWith) }} />
+            <HookEditorLine key='line-sens' caption='sens:' value={sens} onChange={(newSens) => { setSens(newSens) }} />
             <button className='HookEditor-save-btn' onClick={() => { onSave() }} >save</button>
         </div>
     );
